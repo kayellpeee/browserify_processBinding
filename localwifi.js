f036f751e5e460c5ec87ef77ae35c5cc3f46f497
Keenan Lidral-Porter <klp2012@gmail.com>
2014-11-07 10:54:18 -0800

    runs properly on command line with node

--- localwifi.js
+++ localwifi.js
@@ -1,26 +1,27 @@
-var net = require('net');
-var os = require('os');
-var child = require('child_process');
-var exec = require('child_process').exec;
+var os = require('./lib/os.js');
+var child = require('./lib/child_process.js');
+// var child = require('child_process');
 
 var interfaces = os.networkInterfaces();
 var myIp = interfaces.en0[1].address;
+var exec = child.exec;
+var spawn = child.spawn;
 
-child = exec("arp -a", function (error, stdout, stderr) {
+var arp = exec("arp -a", function (error, stdout, stderr) {
   // sys.print('\nstdout: ' + stdout);
   // sys.print('\nstderr: ' + stderr);
   var inet4Addresses = stdout.match(/\([^)]+\)/g);
-  console.log('\n ------- \n', inet4Addresses, '\n ------ \n');
-  console.log(inet4Addresses.length, '\n ------ \n');
-    if (error !== null) {
-    console.log('exec error: ' + error);
-  }
+  console.log('\n ------- \n', inet4Addresses, '\n -----');
+  inet4Addresses.forEach(function(address){
+    var newItem = document.createElement('li');
+    var newText = document.createTextNode(address);
+    newItem.appendChild(newText);
+    document.getElementsByTagName('li')[0].appendChild(newItem);
+  });
+  alert(myIp);
+  // console.log('localhost: ', myIp);
+  // console.log(inet4Addresses.length);
+    // if (error !== null) {
+    // console.log('exec error: ' + error);
+  // }
 });
-
-// console.log(myIp);
-// console.log(interfaces);
-// console.log(path);
-// console.log(process);
-// console.log(url.Url('127.0.0.1'));
-// console.log(environment);
-// console.log(user);
